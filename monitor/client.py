539c24c7ef9eed4674e65b954870dcaede7b6e0b
djshell <djshell@sina.com>
2014-07-28 10:22:53 +0800

    add commit

--- monitor/client.py
+++ monitor/client.py
@@ -3,7 +3,20 @@
 import socket  
 import time
 import sys
+import multiprocessing
 
+pool = multiprocessing.Pool(processes=4)
+result = []
+
+for url in urls:
+   
+    result.append(pool.apply_async(readhtml, (url, )))
+pool.close()
+pool.join()
+for res in result:
+    print res.get()
+
+    
 def clint():
      host = '203.90.236.112'
      port = 32777
@@ -12,13 +25,9 @@ def clint():
      client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
      client.connect(addr)
 
-
           
      data ='中国人 7888888'                  
      client.send(data)
-
-    
-
 
      client.close() 
 
